c7d9e4e7e22faf95b1dfe05bdca3993ae9a9da30
unknown <pchyo92@gmail.com>
2020-03-24 17:17:48 +0900

    commit

--- src/server.js
+++ src/server.js
@@ -1,12 +1,14 @@
 import express from "express";
 import { join } from "path";
 import socketIO from "socket.io";
+import logger from "morgan";
 
 const PORT = 4000;
 const app = express();
 
 app.set("view engine", "pug");
 app.set("views", join(__dirname, "views"));
+app.use(logger("dev"));
 app.use(express.static(join(__dirname, "static")));
 
 app.get("/", (req, res) => {
@@ -19,4 +21,8 @@ const handleListening = () => {
 
 const server = app.listen(PORT, handleListening);
 
-const io = socketIO(server);
+const io = socketIO.listen(server);
+
+io.on("connection", socket => {
+  socket.emit("hello");
+});
